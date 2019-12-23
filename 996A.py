9629911042e36a6e0aabe7ca3eb95673e95ad4d4
Khanh Nguyen <npkhanh93@gmail.com>
2019-12-23 23:05:54 +0700

    implement 996A

--- 996A.py
+++ 996A.py
@@ -1,1 +1,16 @@
 n = int(input())
+
+res = 0
+
+t, n = divmod(n, 100)
+res+=t
+t, n = divmod(n, 20)
+res+=t
+t, n = divmod(n, 10)
+res+=t
+t, n = divmod(n, 5)
+res+=t
+t, n = divmod(n, 1)
+res+=t
+
+print(res)
