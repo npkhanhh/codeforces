0289b531258d1bc41b4f9c07a30f44919ac850ec
Khanh Nguyen <npkhanh93@gmail.com>
2020-03-19 06:12:58 +1100

    add python 352B

---
+++ python/352B.py
@@ -0,0 +1,23 @@
+input()
+a = list(map(int, input().split()))
+
+d = {}
+count = 0
+for i, v in enumerate(a):
+    if v not in d:
+        d[v] = (0, i)
+        count += 1
+    elif d[v]:
+        progress, last_index = d[v]
+        if progress == 0:
+            d[v] = (i - last_index, i)
+        elif i - last_index != progress:
+            d[v] = False
+            count -= 1
+        else:
+            d[v] = (progress, i)
+print(count)
+for key in sorted(d.keys()):
+    if d[key]:
+        print(*[key, d[key][0]])
+
