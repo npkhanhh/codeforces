a056992f7d94cfd46a25b7e9dc1eaa5fd618aa24
Khanh Nguyen <npkhanh93@gmail.com>
2021-02-27 19:42:44 +1100

    add python lib

--- python/lib.py
+++ python/lib.py
@@ -1,4 +1,5 @@
 import queue
+import math
 
 
 def sieve(size):
@@ -117,6 +118,16 @@ def min_reverse(a, n):
             if "".join(r) not in d:
                 d["".join(r)] = 1
                 q.put((r, p[1] + 1))
+
+def divisorGenerator(n):
+    large_divisors = []
+    for i in range(1, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            yield i
+            if i * i != n:
+                large_divisors.append(n // i)
+    for divisor in reversed(large_divisors):
+        yield divisor
 
 
 def suffix_array(s):
