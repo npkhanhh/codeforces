e4e5d8ad4f81b9c2819999e527b7d7f7b3e1e24d
Khanh Nguyen <npkhanh93@gmail.com>
2021-05-03 00:07:12 +1000

    add python 1511A

--- python/educational107/1511A.py
+++ python/educational107/1511A.py
@@ -2,4 +2,4 @@ from sys import stdin
 
 for _ in range(int(stdin.readline())):
     n = int(stdin.readline())
-    print(len(filter(lambda x: x!= 2, map(int, stdin.readline().split()))))
+    print(len(list(filter(lambda x: x != 2, map(int, stdin.readline().split())))))
