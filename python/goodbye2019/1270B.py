3a0e56d124f7e2f48eae807fd59aebd36417cedb
Khanh Nguyen <npkhanh93@gmail.com>
2021-05-12 08:12:53 +1000

    reformat file

--- python/goodbye2019/1270B.py
+++ python/goodbye2019/1270B.py
@@ -1,10 +1,5 @@
 from sys import stdin
 
-if 0:
-    print('hehe')
-else:
-    print('hoho')
-
 for _ in range(int(stdin.readline())):
     n = int(stdin.readline())
     a = list(map(int, stdin.readline().split()))
