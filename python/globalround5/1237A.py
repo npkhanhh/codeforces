bf768d65f50dc8a012adf7b08f163d04c7e745a2
Khanh Nguyen <npkhanh93@gmail.com>
2021-09-17 22:56:15 +1000

    refactor python 1237A

--- python/globalround5/1237A.py
+++ python/globalround5/1237A.py
@@ -1,28 +1,12 @@
 import math
 from sys import stdin
 
-positive = True
-negative = False
+flag = 1
 
 for _ in range(int(stdin.readline())):
     n = int(stdin.readline())
-    if n >= 0:
-        if n % 2 == 0:
-            print(n//2)
-        elif positive:
-            print(int(math.ceil(n/2)))
-            positive = False
-        else:
-            print(int(math.floor(n/2)))
-            positive = True
+    if n % 2 == 0:
+        print(n // 2)
     else:
-        if n % 2 == 0:
-            print(n//2)
-        elif negative:
-            print(int(math.ceil(n/2)))
-            negative = False
-        else:
-            print(int(math.floor(n/2)))
-            negative = True
-
-
+        print(n // 2 + flag)
+        flag = 1 - flag
