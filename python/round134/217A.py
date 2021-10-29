6c606761c91887593f4ceb1f818a57d2809e12c5
Khanh Nguyen <npkhanh93@gmail.com>
2021-10-29 20:33:57 +1100

    edit python 217A

--- python/round134/217A.py
+++ python/round134/217A.py
@@ -1,9 +1,33 @@
 from sys import stdin
+import random
 
-for _ in range(int(stdin.readline())):
-    n, m, k = list(map(int, stdin.readline().split()))
-    if k == n-1 + n*(m-1):
-        print('YES')
+n = int(stdin.readline())
+a = []
+groups = []
+cur_group = 1
+for i in range(n):
+    x, y = list(map(int, stdin.readline().split()))
+    new_group = set()
+    for j in range(len(a)):
+        if x == a[j][0] or y == a[j][1]:
+            new_group.add(groups[j])
+    a.append([x, y])
+    if len(new_group) == 0:
+        groups.append(cur_group)
+        cur_group += 1
+    elif len(new_group) >= 2:
+        main = new_group.pop()
+        for idx, val in enumerate(groups):
+            if val in new_group:
+                groups[idx] = main
+        groups.append(main)
     else:
-        print('NO')
+        groups.append(new_group.pop())
+print(len(set(groups))-1)
+
+
+
+
+
+
 
