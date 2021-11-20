fc12a4ea795c9a0787450e74cdf2cc6e898eb69f
Khanh Nguyen <npkhanh93@gmail.com>
2021-11-20 23:27:46 +1100

    add python 1584B

--- python/technocup2022-elimround2/1584B.py
+++ python/technocup2022-elimround2/1584B.py
@@ -6,11 +6,13 @@ def cal(d1, d2):
         return 1
     if d1 == 2 and d2 == 2:
         return 2
-    d1, d2 = sorted([d1, d2])
+    t = d1*d2
+    d1, d2 = sorted([d1, d2], reverse=True)
     if d1 > 1:
         a, b = divmod(d1, 3)
         t = d2 * a
-        t += cal(d2, b)
+        if b > 0:
+            t += cal(d2, b)
     return t
 
 
